c337cc1d99310de2e3fb905310f30637a3d89f3f
themysterysolver <arjunprabhakar1910@gmail.com>
2024-08-18 08:18:55 +0530

    FEAT:error without importing package

---
+++ WEEK-4/DEMO-6/pack1/hello.java
@@ -0,0 +1,6 @@
+class hello{
+    public static void main(String[] args){
+       a b=new a();
+       a.print(); 
+    }
+}
---
+++ WEEK-4/DEMO-6/pack1/pack2/a.java
@@ -0,0 +1,5 @@
+public class a{
+    void print(){
+        System.out.println("Hello world!!");
+    }
+}
