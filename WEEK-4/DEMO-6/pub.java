cd5518edd99f1b3807779e9dd48c2c05523d72dd
themysterysolver <arjunprabhakar1910@gmail.com>
2024-08-18 08:34:18 +0530

    FEAT:no need to seperate public class files!

---
+++ WEEK-4/DEMO-6/pub.java
@@ -0,0 +1,11 @@
+public class A{
+    public void print(){
+        System.out.println("Hello world!!");
+    }
+}
+class pub{
+    public static void main(String[] args){
+        A b=new A();
+        b.print();
+    }
+}
