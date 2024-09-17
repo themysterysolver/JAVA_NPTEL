49af9be034e61796ddf3c68f109c56e70c6efb20
themysterysolver <arjunprabhakar1910@gmail.com>
2024-09-17 10:21:01 +0530

    FEAT:some new functiosn on FILES

--- WEEK-7/forFile.java
+++ WEEK-7/forFile.java
@@ -7,5 +7,12 @@ class forFile{
         System.out.println("name:"+toCheck.getName());
         System.out.println("path:"+toCheck.getPath());
         System.out.println("parent:"+toCheck.getParent());
+        File f=toCheck;
+        if(f.exists()){
+            System.out.println("READABLE:"+f.canRead());
+            System.out.println("WRITABLE:"+f.canWrite());
+            System.out.println("LAST MODIFIED ON:"+f.lastModified());
+            System.out.println("FILE IN BYTES:"+f.length());
+        }
     }
 }
