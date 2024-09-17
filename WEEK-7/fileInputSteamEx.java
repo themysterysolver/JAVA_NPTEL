c8df1c7493300057c017b5e2bbd2c8aa3d41aaeb
themysterysolver <arjunprabhakar1910@gmail.com>
2024-09-17 10:38:39 +0530

    FEAT:fileInputStream handling

---
+++ WEEK-7/fileInputSteamEx.java
@@ -0,0 +1,19 @@
+import java.io.FileInputStream;
+import java.io.*;
+class fileInputSteamEx{
+    public static void main(String[] args){
+        String name="hello.txt";
+        try{
+            FileInputStream f=new FileInputStream(name);
+            int content;
+            while((content=f.read())!=-1){
+                System.out.print((char)content);
+            }
+            f.close();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    
+}
