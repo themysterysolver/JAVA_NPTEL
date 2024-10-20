26da8a33b19c9448b68bb462679926d0f1501574
themysterysolver <arjunprabhakar1910@gmail.com>
2024-10-20 13:16:30 +0530

    FEAT:my pannel is add to frame!

---
+++ WEEK-8/MyPanel.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+
+public class MyPanel {
+    public static void main(String[] args) {
+        Frame f = new Frame("My first use of frame!!");
+        f.setSize(250, 250);              // Set the size of the frame
+        f.setBackground(Color.CYAN);
+        f.setLayout(null);                // Use null layout for manual positioning
+
+        Panel p = new Panel();
+        p.setBounds(100, 100, 50, 50);    // Set the position and size of the panel
+        p.setBackground(Color.ORANGE);
+
+        f.add(p);                         // Add panel to the frame
+        f.setVisible(true);               // Make the frame visible
+    }
+}
