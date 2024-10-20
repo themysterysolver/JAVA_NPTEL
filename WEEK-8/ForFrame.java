846ac713d8f5452a6b3e236d4ff961a7c47c28c8
themysterysolver <arjunprabhakar1910@gmail.com>
2024-10-20 12:56:26 +0530

    FEAT:my first frame

---
+++ WEEK-8/ForFrame.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+public class ForFrame {
+    public static void main(String[] args) {
+        Frame f=new Frame("My first use of frame!!");
+        f.resize(250,250);
+        f.setBackground(Color.CYAN);
+        f.show();
+    }
+}
